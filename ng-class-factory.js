f363fb68307ea09ac1029ed1abe5b2fe3d61c56c
monis01 <mnskhn7997@gmail.com>
2016-12-25 16:48:30 +0530

    add,remove,reset,forceNull functions added

--- ng-class-factory.js
+++ ng-class-factory.js
@@ -1,5 +1,7 @@
 angular.module('ngClassFactory', [])
     .factory('classFactory', [function() {
+        var error = {}
+
         function classObject() {
             if (arguments.length) {
                 for (var i in arguments) {
@@ -12,19 +14,70 @@ angular.module('ngClassFactory', [])
         classObject.prototype.addKeys = addKeys;
         classObject.prototype.removeKeys = removeKeys;
         classObject.prototype.resetKeys = resetKeys;
-        classObject.prototype.forceResetToNull  = forceResetToNull;
+        classObject.prototype.forceResetToNull = forceResetToNull;
 
-        function addKeys(){
-
+        function addKeys() {
+            var self = this;
+            try {
+                if (arguments.length) {
+                    for (var i in arguments) {
+                        self[i] = ''
+                    }
+                } else {
+                    error.code = 1;
+                    error.msg = "Keys are missing"
+                    throw error
+                }
+            } catch (e) {
+                console.log(e);
+            }
+            return self;
         }
-        function removekeys(){
 
+        function removekeys() {
+            var self = this;
+            try {
+                if (arguments.length) {
+                    for (var i in arguments) {
+                        if (Object.keys(self).indexOf(arguments[i]) != -1) {
+                            delete self[arguments[i]];
+                        }
+                    }
+                    return self;
+                } else {
+                    error.code = 1;
+                    error.msg = "Keys are missing"
+                    throw error
+                }
+            } catch (e) {
+                console.log(e.message);
+            }
         }
-        function resetKeys(){
 
+        function resetKeys() {
+            //@ if keys in arguments will be provied, then those keys will be resetted
+            //@ else whole keys will get reset
+            var self = this;
+            if (arguments.length) {
+                resetMe(arguments);
+            } else {
+                resetMe(Object.keys(self));
+            }
+
+            function resetMe(keysVal) {
+                for (var i in keysVal) {
+                    self[i] = '';
+                }
+            }
+            return self;
         }
-        function forecResetToNull(){
-            
+
+        function forecResetToNull() {
+            var self = this;
+            self.forEach(function(value, key) {
+                self[key] = null;
+            })
+            return self;
         }
         return classObject;
     }])
