98ade4cfa8c0431801323738190e7bef490dcec1
monis01 <mnskhn7997@gmail.com>
2017-07-23 16:40:16 +0530

    added header

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-angular.module('demoApp',[])
+angular.module('demoApp',['ngRoute'])
+.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            // .when('/', {
+            //     templateUrl : 'templates/home.html',
+            //     controller  : 'homeController'
+            // })
+
+            // // route for the search page
+            // .when('/search-recipie', {
+            //     templateUrl : 'templates/searchRecipie.html',
+            //     controller  : 'searchController'
+            // })
+})
